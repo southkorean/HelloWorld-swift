e5c75ead308f796dfebb394d48211ac816c75524
southkorean <peppi@knou.ac.kr>
2021-05-02 17:02:41 +0900

    * 개발자 모드 * 버튼 기능 추가 개발합니다.

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var clickBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,6 +18,9 @@ class ViewController: UIViewController {
         textLabel.text = "헬로우 월드입니다."
     }
 
-
+    @IBAction func btnAction(_ sender: Any) {
+        textLabel.text = "바꿨습니다."
+    }
+    
 }
 
